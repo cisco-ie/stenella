38efe7c6cf49c73848c14e38db03c0667a53df19
Brandon Him <brh55@drexel.edu>
2016-10-19 13:28:01 -0700

    Refactor and add event route

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -6,26 +6,52 @@
 var express        = require('express'),
   router           = express.Router(),
   parseHeaders     = require('../services/AdministerChannels').parseHeaders;
+  EventsController = require('../controller/EventsController');
 
 router.post('/', function (request, response) {
   var headers = parseHeaders(request)
   // More information regarding syncs:
   // https://developers.google.com/google-apps/calendar/v3/push
-  if (headers.channelId && headers.resourceId) {
-    // Initial Successful Sync
-    if (headers.resourceState === 'sync') {
+  if (isWatchNotification(headers)) {
+    if (isInitialSync(headers)) {
       var message = headers.channelId + ' channel has been established.';
       console.log(message);
-    // A request regarding a Notification update
-    } else if (headers.resourceState === 'exists') {
+    }
+    if (isEventUpdate(headers)) {
       EventsController.load(headers.channelId);
     }
-    // Respond to Server that it is a success
     response.sendStatus(200);
   } else {
-    // POST request not regarding notifications should not be sent here
     response.sendStatus(400);
   }
 });
+
+/**
+ * Query if Channel Watch Notification
+ * @param  {object}  parseHeaders parse request headers
+ * @return {Boolean}
+ */
+function isWatchNotification(parseHeaders) {
+  return (parseHeaders.channelId && parseHeaders.resourceId);
+}
+
+/**
+ * Query if the request is the initial confirmation
+ * @param  {object}  parseHeaders parse request headers
+ * @return {Boolean}
+ */
+function isInitialSync(parseHeaders) {
+  return (parseHeaders.resourceState === 'sync');
+}
+
+/**
+ * Query if the request is an event update
+ * @param  {object}  parseHeaders parse request headers
+ * @return {Boolean}                [description]
+ */
+function isEventUpdate(parseHeaders) {
+  return (parseHeaders.resourceState === 'exists');
+}
+
 
 module.exports = router;
