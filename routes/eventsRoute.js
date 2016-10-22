31c2e7d49ea1610d259bc5af17550e001a2d96b9
Brandon Him <brh55@drexel.edu>
2016-10-22 16:13:44 -0700

    Refactor events route

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -13,23 +13,28 @@ var eventController = require('../controllers/eventController');
  */
 router.post('/', function (request, response) {
   var headers = parseHeaders(request)
-  // More information regarding syncs:
-  // https://developers.google.com/google-apps/calendar/v3/push
-  if (isWatchNotification(headers)) {
-    if (isInitialSync(headers)) {
-      var message = headers.channelId + ' channel has been established.';
-      console.log(message);
-    }
-    if (isEventUpdate(headers)) {
-      eventController.load(headers.channelId);
-    }
-    response.sendStatus(200);
-  } else {
+  // More information: https://developers.google.com/google-apps/calendar/v3/push
+  if (!isWatchNotification(headers))
     response.sendStatus(400);
-  }
+  parseNotificationAndRespond(headers);
 });
 
+
 module.exports = router;
+
+/**
+ * This takes in a notification and responds accordingly
+ * @param  {Object} parseHeaders parse headers of the notification
+ * @return {Void}
+ */
+function parseNotificationAndRespond(parseHeaders) {
+    // No parsing needed for a confirm notification
+    if (isInitialSync(headers))
+      console.log(headers.channelId + ' channel has been established.');
+    if (isEventUpdate(headers))
+      eventController.load(headers.channelId);
+    response.sendStatus(200);
+}
 
 /**
  * Query if Channel Watch Notification
