29f8867462f4b7640342f964d5b4acd7c254a735
Brandon Him <brh55@drexel.edu>
2016-11-23 18:16:30 -0800

    Add pagination to user listing request

--- services/AdministerUsers.js
+++ services/AdministerUsers.js
@@ -2,8 +2,11 @@
 
 var google    = require('googleapis');
 var _         = require('lodash');
+var scope     = require('../constants/GoogleScopes');
 var directory = google.admin('directory_v1');
 var config    = require('../configs/config');
+var Promise   = require('bluebird');
+var createJWT = require('../services/AdministerJWT').createJWT;
 
 var Interface = {
   list: getUsers
@@ -13,17 +16,54 @@ module.exports = Interface;
 
 /**
  * Invoke google api to get directory of users
- * @param  {object}   authToken <required> secure JWT for user directories
- * @param  {object}   userParams <optional> parameters to retrieve users
- * @param  {Function} callback   for google's directory response
- * @return {function} invokes callback
+ * @param  {object}   overrideParams <optional> parameters to retrieve users
+ * @return {object} a promise that resolves into a user listing response
  */
-function getUsers(authToken, userParams, callback) {
-  // @TODO: add support for pagination under Utilities
-  // @TODO: consider customer id in liu of domain & introduce page token
-  if (!authToken) return callback(new Error('No auth token provided'));
+function getUsers(overrideParams) {
+  return new Promise(function userPromise(resolve, reject) {
+    createJWT(scope.userDirectory)
+      .then(function authorizeJwtResponse(jwtClient) {
+        var params = buildParams(jwtClient, overrideParams);
+        requestUserList(params)
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(console.log);
+  });
+}
+
+function requestUserList(params) {
+  var directoryList = Promise.promisify(directory.users.list);
+
+  // Returns a user listing response in the
+  // cases of pagination, the response will
+  // paginate, and append the users into
+  // a single response
+  return directoryList(params)
+    .then(function directoryListResponse(userResponse) {
+      if (userResponse.nextPageToken) {
+        var pageToken = userResponse.nextPageToken;
+        params.pageToken = pageToken;
+        // @TODO: Consider optimizing by returning
+        // multiple response async
+        return requestUserList(params)
+          .then(function mergeResponse(paginatedResponse) {
+            var mergeUsers = _.concat(userResponse.users,
+                                      paginatedResponse.users);
+            userResponse.users = mergeUsers;
+            return userResponse;
+          });
+      }
+      return userResponse;
+    })
+    .catch(function handleListError(error) {
+      throw error;
+    });
+}
+
+function buildParams(jwtClient, overrideParams) {
   var defaultParams = {
-    auth: authToken,
+    auth: jwtClient,
     maxResults: 500,
     orderBy: 'email'
   };
@@ -34,6 +74,6 @@ function getUsers(authToken, userParams, callback) {
     defaultParams.domain = config.domain;
   }
 
-  var params = _.extend(defaultParams, userParams);
-  return directory.users.list(params, callback);
+  var params = _.extend(defaultParams, overrideParams);
+  return params;
 }
