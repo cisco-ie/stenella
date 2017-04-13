ef022beaf357c36e91300f69371c4b8780aa0ed0
Brandon Him <brh55@drexel.edu>
2017-04-13 15:51:10 -0700

    Update config for es6

--- configs/config.js
+++ configs/config.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var _ = require('lodash');
+const _ = require('lodash');
 require('dotenv').config();
 
-var RECEIVING_URL = normalizeUrl(process.env.RECEIVING_URL);
+const RECEIVING_URL = normalizeUrl(process.env.RECEIVING_URL);
 
-var configs = {
+const configs = {
   port: Number(process.env.PORT) || 5000,
   authorizeAdmin: process.env.ADMIN,
   receivingUrl: {
@@ -13,6 +13,24 @@ var configs = {
     events: RECEIVING_URL + '/watch/events',
     users: RECEIVING_URL + '/watch/users'
   }
+};
+
+const throwUndefined = (value, key) => {
+  if (!value) {
+    throw new Error(key + ' is not defined in .env file');
+  }
+};
+
+const normalizeUrl = (receivingUrl) => {
+  if (typeof receivingUrl !== 'string') {
+    throw new Error('Receiving URL is not defined');
+  }
+
+  // Check for trailing back slash
+  if (receivingUrl.charAt(receivingUrl.length - 1) === '/') {
+    return receivingUrl.substring(0, receivingUrl.length - 1);
+  }
+  return receivingUrl;
 };
 
 if (process.env.DOMAIN) {
@@ -30,23 +48,5 @@ if (process.env.TTL) {
 _.forOwn(configs, function iterateConfigKeys(value, key) {
   if (!configs[key]) throwUndefined(value, key);
 });
-
-function throwUndefined(value, key) {
-  if (!value) {
-    throw new Error(key + ' is not defined in .env file');
-  }
-}
-
-function normalizeUrl(receivingUrl) {
-  if (typeof receivingUrl !== 'string') {
-    throw new Error('Receiving URL is not defined');
-  }
-
-  // Check for trailing back slash
-  if (receivingUrl.charAt(receivingUrl.length - 1) === '/') {
-    return receivingUrl.substring(0, receivingUrl.length - 1);
-  }
-  return receivingUrl;
-}
 
 module.exports = configs;
