99fdba238195124262fc9a611294b48b52be19af
Brandon Him <brh55@drexel.edu>
2017-05-08 11:20:50 -0700

    Clean up example observer

--- observers/exampleObserver.js
+++ observers/exampleObserver.js
@@ -2,6 +2,5 @@ const calendarEvents = require('../controllers/eventController').observable;
 calendarEvents.subscribe(catchPTOs);
 
 function catchPTOs (calendarEvent) {
-  console.log(calendarEvent);
   if (calendarEvent.summary.match(/PTO/g)) console.log('%s is on PTO', calendarEvent.userId);
 }
