e3d6734bc4c399a5f051fbe2121b36ca51906bd5
brh55 <brandonhim@live.com>
2017-06-14 16:30:24 -0500

    Update constants to es6 syntax

--- constants/GoogleScopes.js
+++ constants/GoogleScopes.js
@@ -1,13 +1,12 @@
-var baseUrl = 'https://www.googleapis.com/auth/';
+const baseUrl = 'https://www.googleapis.com/auth/';
 
 /**
  * Google Calendar API url   https://developers.google.com/google-apps/calendar/
  * Google Calendar User Directory  https://developers.google.com/admin-sdk/
  */
-var scopeUrls = {
+const scopeUrls = {
   calendar: baseUrl + 'calendar',
   userDirectory: baseUrl + 'admin.directory.user'
 };
 
 module.exports = scopeUrls;
-
